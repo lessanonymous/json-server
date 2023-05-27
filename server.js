44c8691f72b11085faaac22248f76167429d106b
Lesley Oehlers <lesleyoehlers@MacBook-Pro-van-Lesley.local>
2023-05-27 13:03:18 +0200

    added cors

--- server.js
+++ server.js
@@ -1,8 +1,9 @@
 // JSON Server module
 const jsonServer = require("json-server");
+const cors = require("cors");
 const server = jsonServer.create();
 const router = jsonServer.router("db/db.json");
-const middlewares = jsonServer.defaults({noCors: true});
+const middlewares = jsonServer.defaults();
 
 server.use(middlewares);
 // Add this before server.use(router)
@@ -12,6 +13,7 @@ server.use(
 		"/api/*": "/$1",
 	})
 );
+server.use(cors());
 server.use(router);
 server.listen(3000, () => {
 	console.log("JSON Server is running");
